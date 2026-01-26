16c6cede9e215d2913cae5940b6cf01a9efd6965
ShaneBeee <shanebolenback@me.com>
2026-01-26 08:09:26 -0800

    DefaultFunctions - fix mod func to take in number not int

--- src/main/java/io/github/syst3ms/skriptparser/structures/functions/DefaultFunctions.java
+++ src/main/java/io/github/syst3ms/skriptparser/structures/functions/DefaultFunctions.java
@@ -5,14 +5,14 @@ public class DefaultFunctions {
     static {
         Functions.registerFunction(new JavaFunction<>(
                 "mod",
-                new FunctionParameter[]{new FunctionParameter<>("i", Integer.class, true), new FunctionParameter<>("m", Integer.class, true)},
+                new FunctionParameter[]{new FunctionParameter<>("i", Number.class, true), new FunctionParameter<>("m", Number.class, true)},
                 Number.class,
                 true) {
             @Override
             public Number[] executeSimple(Object[][] params) {
                 Number d = (Number) params[0][0];
                 Number m = (Number) params[1][0];
-                return new Number[] {d.intValue() % m.intValue()};
+                return new Number[] {d.doubleValue() % m.doubleValue()};
             }
         });
     }
