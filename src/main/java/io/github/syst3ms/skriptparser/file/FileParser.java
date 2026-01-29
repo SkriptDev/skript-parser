338dfe545ac770aa75c3da0d4dd9fc6a30a74be1
ShaneBeee <shanebolenback@me.com>
2026-01-29 15:30:42 -0800

    FileParser - redo comment removal - allow comments within strings

--- src/main/java/io/github/syst3ms/skriptparser/file/FileParser.java
+++ src/main/java/io/github/syst3ms/skriptparser/file/FileParser.java
@@ -3,8 +3,6 @@ package io.github.syst3ms.skriptparser.file;
 import io.github.syst3ms.skriptparser.log.ErrorType;
 import io.github.syst3ms.skriptparser.log.SkriptLogger;
 import io.github.syst3ms.skriptparser.util.FileUtils;
-import io.github.syst3ms.skriptparser.util.color.Color;
-import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,18 +12,20 @@ import java.util.regex.Pattern;
  * A class for parsing a plaintext file into a list of {@link FileElement}s representing every line.
  */
 public class FileParser {
-    public static final Pattern LINE_PATTERN = Pattern.compile("^((?:[^#]|##)*)(\\s*#(?!#).*)$"); // Might as well take that from Skript
+
+    public static final Pattern COMMENT_PATTERN = Pattern.compile("#(?=(?:(?:[^\"]*\"){2})*[^\"]*$)");
 
     /**
      * Parses a {@linkplain List} of strings into a list of {@link FileElement}s. This creates {@link FileElement} and
      * {@link FileSection} objects from the lines, effectively structuring the lines into a tree.
      * This removes comments from each line, and discards any blank lines afterwards.
-     * @param fileName the name of the file the lines belong to
-     * @param lines the list of lines to parse
+     *
+     * @param fileName            the name of the file the lines belong to
+     * @param lines               the list of lines to parse
      * @param expectedIndentation the indentation level the first line is expected to be at
-     * @param lastLine a parameter that keeps track of the line count throughout recursive calls of this method when
-     *                 parsing sections
-     * @param logger the logger
+     * @param lastLine            a parameter that keeps track of the line count throughout recursive calls of this method when
+     *                            parsing sections
+     * @param logger              the logger
      * @return a list of {@link FileElement}s
      */
     public static List<FileElement> parseFileLines(String fileName, List<String> lines, int expectedIndentation, int lastLine, SkriptLogger logger) {
@@ -34,9 +34,7 @@ public class FileParser {
             var line = lines.get(i);
             String content = removeComments(line);
 
-            if (content == null) {
-                content = line.replace("##", "#").strip();
-            } else if (content.isEmpty()) {
+            if (content.isEmpty()) {
                 elements.add(new VoidElement(fileName, lastLine + i, expectedIndentation));
                 continue;
             }
@@ -44,9 +42,9 @@ public class FileParser {
             var lineIndentation = FileUtils.getIndentationLevel(line, false);
             if (lineIndentation > expectedIndentation) { // The line is indented too much
                 logger.error(
-                        "The line is indented too much (line " + (lastLine + i) + ": \"" + content + "\")",
-                        ErrorType.STRUCTURE_ERROR,
-                        "You only need to indent once (using tabs) after each section (a line that ends with a ':'). Try to omit some tabs so the line suffices this rule"
+                    "The line is indented too much (line " + (lastLine + i) + ": \"" + content + "\")",
+                    ErrorType.STRUCTURE_ERROR,
+                    "You only need to indent once (using tabs) after each section (a line that ends with a ':'). Try to omit some tabs so the line suffices this rule"
                 );
                 continue;
             } else if (lineIndentation < expectedIndentation) { // One indentation behind marks the end of a section
@@ -55,14 +53,14 @@ public class FileParser {
             if (content.endsWith(":")) {
                 if (i + 1 == lines.size()) {
                     elements.add(new FileSection(fileName, lastLine + i, content.substring(0, content.length() - 1),
-                            new ArrayList<>(), expectedIndentation
+                        new ArrayList<>(), expectedIndentation
                     ));
                 } else {
                     var sectionElements = parseFileLines(fileName, lines.subList(i + 1, lines.size()),
-                            expectedIndentation + 1, lastLine + i + 1,
-                            logger);
+                        expectedIndentation + 1, lastLine + i + 1,
+                        logger);
                     elements.add(new FileSection(fileName, lastLine + i, content.substring(0, content.length() - 1),
-                            sectionElements, expectedIndentation
+                        sectionElements, expectedIndentation
                     ));
                     i += count(sectionElements);
                 }
@@ -87,44 +85,18 @@ public class FileParser {
 
     /**
      * Removes all comments from a given String
+     *
      * @param string the String
      * @return the String with the comments removed; {@literal null} if no comments were found; an empty string if the whole String was a comment
      */
-    @Nullable
     private static String removeComments(String string) {
+        if (!string.contains("#")) return string.strip();
         if (string.matches("^[\\s\\t]*#[^#]+") || string.startsWith("#") || string.isBlank()) {
             return ""; // Whole string is a comment
         }
 
-        var builder = new StringBuilder();
-        outer:
-        for (int i = 0; i < string.length(); i++) {
-            char c = string.charAt(i);
-
-            if (c == '#') {
-                if (string.charAt(i + 1) == '#') {
-                    builder.append(c).append(string.charAt(++i));
-                } else {
-                    var checked = string.substring(i + 1);
-                    for (int j : new int[] {3, 6, 8}) {
-                        if (checked.length() >= j
-                                && Color.COLOR_PATTERN.matcher(checked.substring(0, j)).matches()) {
-                            builder.append(c).append(checked, 0, j);
-                            i+=j;
-                            continue outer;
-                        }
-                    }
-
-                    // Comment was found. Erase it from the string
-                    assert !builder.toString().isEmpty();
-                    return builder.toString().strip();
-                }
-            } else {
-                builder.append(c);
-            }
-        }
-        if (builder.toString().equals(string))
-            return null;
-        return builder.toString().strip();
+        String[] split = string.split(COMMENT_PATTERN.pattern());
+        return split[0].strip();
     }
+
 }
